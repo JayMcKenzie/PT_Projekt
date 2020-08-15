e33a24e69966ddbf09b41b3e7082183155eb21a4
hasashin <dominik.hazak@gmail.com>
2020-08-15 17:25:48 +0200

    modified game files

--- Rules/src/game/Game.kt
+++ Rules/src/game/Game.kt
@@ -1,37 +1,19 @@
 package game
 
-import gameAI.AI
-import gameAI.ArtificialIntelligence
 import sample.Controller
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
 open class Game(player1: Player, player2: Player, private val controller: Controller) : GameCore(player1, player2), Runnable {
 
-    private var ai: ArtificialIntelligence? = null
     private var startPlayer : Player? = null
     private var gameThread: Thread? = null
 
-    init {
-        if (player1.type == PlayerType.AI || player2.type == PlayerType.AI) {
-            ai = AI(this)
-        }
-    }
 
     override fun start() {
         prepareBoard()
         prepareGame()
         startPlayer = if (Random.nextInt(1, 3) == 1) player1 else player2
-        when (startPlayer) {
-            player1 -> {
-                ai?.filename = "p1start.bin"
-                ai?.load()
-            }
-            player2 -> {
-                ai?.filename = "p2start.bin"
-                ai?.load()
-            }
-        }
         currentPlayer = startPlayer
         gameThread = thread {
             try {
@@ -49,46 +31,23 @@ open class Game(player1: Player, player2: Player, private val controller: Contro
         var lastCapturePawn = -1
         while (!gameOver) {
             checkCaptureObligation()
-            controller.zmienGracza(currentPlayer!!.number)
             ruch@while (true) {
-                controller.koloruj(matrix)
-                val nextMove = when (currentPlayer!!.type) {
-                    PlayerType.AI -> {
-                        val startFields: Array<Int>
-                        startFields = if (captureRequired) {
-                            val temp = mutableListOf<Int>()
-                            for (move in captureMoves) {
-                                temp.add(move.first)
-                            }
-                            temp.toTypedArray()
-                        } else {
-                            arrayOf()
-                        }
-                        Thread.sleep(700)
-                        ai!!.move(startFields)
-                    }
-                    PlayerType.Human -> {
-                        controller.zacznijRuch()
-                        var move = controller.pobierzRuch()
-                        while (move == null) {
-                            Thread.sleep(100)
-                            move = controller.pobierzRuch()
-                        }
-                        move
-                    }
-                }
+                controller.fillBoard(matrix)
+                //tutaj trzeba będzie wrzucić ruch z klasy zasad (zmiana macierzy odebranych będzie nowym ruchem)
+
+                val nextMove = getPossibleMoves()[0]  //tylko dla celów niewysrywania się
+                        //var move = controller.pobierzRuch()
+//                        while (move == null) {
+//                            Thread.sleep(100)
+//                            move = controller.pobierzRuch()
+//                        }
+//                        move
                 try {
                     lastMoveWasCapture = captureRequired
                     if(lastMoveWasCapture)
                         lastCapturePawn = nextMove.second
                     move(nextMove.first, nextMove.second)
-                    if (getOponent(currentPlayer)?.type == PlayerType.AI) {
-                        ai?.setEnemyMove(generateBoardHash(), nextMove)
-                    }
                     controller.wyswietlWiadomosc("")
-                    if(currentPlayer?.type == PlayerType.AI){
-                        ai?.update(generateBoardHash())
-                    }
                 } catch (e: InvalidMoveException) {
                     controller.wyswietlWiadomosc("Nie można wykonać ruchu")
                     continue@ruch
@@ -106,14 +65,7 @@ open class Game(player1: Player, player2: Player, private val controller: Contro
             currentPlayer = getOponent(currentPlayer)
             Thread.sleep(100)
         }
-        controller.koloruj(matrix)
+        controller.fillBoard(matrix)
         controller.wyswietlWiadomosc("Koniec gry! Wygrał gracz ${winner!!.number}")
-        ai?.updateState(winner == startPlayer && startPlayer?.type == PlayerType.AI)
-        ai?.save()
-    }
-
-    fun close() {
-        gameThread?.interrupt()
-        ai?.save()
     }
 }
